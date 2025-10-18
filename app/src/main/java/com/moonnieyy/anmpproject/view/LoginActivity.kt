9748dd136c3f01b97fc31f97f8f9fee1bbeda74f
Michelle Christabel <michelle.christabel9@gmail.com>
2025-10-18 11:07:32 +0700

    UI login

--- app/src/main/java/com/moonnieyy/anmpproject/view/LoginActivity.kt
+++ app/src/main/java/com/moonnieyy/anmpproject/view/LoginActivity.kt
@@ -1,4 +1,40 @@
 package com.moonnieyy.anmpproject.view
 
-class LoginActivity {
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.toolbox.Volley
+import com.moonnieyy.anmpproject.databinding.ActivityLoginBinding
+import org.json.JSONObject
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // cek user udah pernah login/belum
+        val sharedPreferences = getSharedPreferences("user_session", MODE_PRIVATE)
+        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+
+        if (isLoggedIn) {
+            // Jika sudah login, arahkan ke MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+            return
+        }
+
+        // kalau blm login, tampilkan halaman login
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+
+        }
 }
