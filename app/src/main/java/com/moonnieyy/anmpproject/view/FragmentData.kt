b566362e8890e3f0e8af1c80aa77929f03470ac6
floren99 <150808616+floren99@users.noreply.github.com>
2025-10-23 23:07:04 +0700

    benerin nav for the last time

--- app/src/main/java/com/moonnieyy/anmpproject/view/FragmentData.kt
+++ app/src/main/java/com/moonnieyy/anmpproject/view/FragmentData.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.moonnieyy.anmpproject.R
 import com.moonnieyy.anmpproject.databinding.FragmentDataBinding
 import com.moonnieyy.anmpproject.model.DataUkur
 import com.moonnieyy.anmpproject.viewmodel.DataViewModel
