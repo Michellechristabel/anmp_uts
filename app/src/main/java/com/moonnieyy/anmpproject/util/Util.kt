967def83fbf33f54666a6ecf7124e88f42b2d469
floren99 <150808616+floren99@users.noreply.github.com>
2026-01-14 17:29:48 +0700

    db baru
    tadi salah nama db coba lagi kawan

--- app/src/main/java/com/moonnieyy/anmpproject/util/Util.kt
+++ app/src/main/java/com/moonnieyy/anmpproject/util/Util.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.room.Room
 import com.moonnieyy.anmpproject.model.AppDatabase
 
-const val DB_NAME = "anmp_uts_db"
+const val DB_NAME = "anmp_uas_db"
 
 fun buildDb(context: Context): AppDatabase {
     return Room.databaseBuilder(
