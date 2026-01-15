245960de0a3e041c128502a82e43a451fb72cbdb
floren99 <150808616+floren99@users.noreply.github.com>
2026-01-15 23:56:39 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
 
     defaultConfig {
         applicationId = "com.moonnieyy.anmpproject"
-        minSdk = 21
-        targetSdk = 34
+        minSdk = 26
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
